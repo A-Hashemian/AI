3922a3d3696080161043b3ba6178cb18d63c3380
A-Hashemian <amin@hashemian.tech>
2022-10-14 18:28:44 +0300

    set_up

---
+++ EXCEL/app.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from PIL import Image
+
+st.set_page_config(page_title='Genomics of Drug Sensitivity in Cancer')
+st.header('Results 2021')
+st.subheader('Read More.')
