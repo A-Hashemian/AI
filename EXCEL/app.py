fd7517b32310241e2b7414389d80fb1061e29003
A-Hashemian <amin@hashemian.tech>
2022-10-16 21:56:12 +0300

    load data

--- EXCEL/app.py
+++ EXCEL/app.py
@@ -1,3 +1,4 @@
+import pandas as pd
 
 import streamlit as st
 
@@ -5,3 +6,17 @@ import streamlit as st
 st.set_page_config(page_title='Genomics of Drug Sensitivity in Cancer')
 st.header('Results 2021')
 st.subheader('Read More.')
+
+excel_file = 'GDSC.xlsx'
+sheet_name = 'DATA'
+
+df = pd.read_excel(excel_file,
+                   sheet_name=sheet_name,
+                   usecols='B:D',
+                   header=3)
+
+df_participants = pd.read_excel(excel_file,
+                                sheet_name= sheet_name,
+                                usecols='F:G',
+                                header=3)
+df_participants.dropna(inplace=True)
